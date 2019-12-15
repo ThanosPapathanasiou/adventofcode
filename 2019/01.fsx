9f3308712d340f3c0b8ea21603fa37636aadbac9
Thanos Papathanasiou <thanospapathanasiou@hotmail.com>
2019-12-15 15:28:06 +0000

    Update 01.fsx
    completed day 1

--- 2019/01.fsx
+++ 2019/01.fsx
@@ -1,7 +1,17 @@
-let input = 
+let calculateFuel moduleMass = 
+
+    let rec calculateFuel' fuel mass = 
+        let fuelForMass = mass / 3 - 2
+        match fuelForMass > 0 with
+        | false -> fuel
+        | true -> calculateFuel' (fuel + fuelForMass) fuelForMass
+
+    calculateFuel' 0 moduleMass
+
+let fuelRequirements = 
     System.IO.File.ReadAllLines(__SOURCE_DIRECTORY__ + "\\" + "01-input.txt")
     |> Array.map int
-    |> Array.map (fun moduleMass -> (moduleMass / 3) - 2)
+    |> Array.map calculateFuel
     |> Array.sum
 
-input
+fuelRequirements
